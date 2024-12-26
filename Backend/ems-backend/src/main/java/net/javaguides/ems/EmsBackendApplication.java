9bdd1079b176a2ed1a764d5850da6beb35f9f318
Siya Kamboj <siyakamboj@Siyas-Air.attlocal.net>
2024-12-25 19:09:00 -0800

    Testing .env

--- Backend/ems-backend/src/main/java/net/javaguides/ems/EmsBackendApplication.java
+++ Backend/ems-backend/src/main/java/net/javaguides/ems/EmsBackendApplication.java
@@ -2,11 +2,14 @@ package net.javaguides.ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class EmsBackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		SpringApplication.run(EmsBackendApplication.class, args);
 	}
 
